1ba3f008f720c2bf5e8236aa7ce39fc0e8fcd9fb
Vednus <sundev.lohr@gmail.com>
2024-10-29 14:46:40 -0600

    feat: add better instructions for handling scanner errors. Add gym map video to documentation

--- src/components/ScannerErrorTable.js
+++ src/components/ScannerErrorTable.js
@@ -16,17 +16,19 @@ const ScannerErrorTable = () => {
           <tr style={{verticalAlign: "top", borderBottom: "1px solid #ccc"}}>
             <td style={{paddingRight: 5, paddingBottom: 5}}><strong>Low Battery</strong></td>
             <td style={{paddingRight: 5, paddingBottom: 5}}>The scanner has a low battery.</td>
-            <td style={{paddingRight: 5, paddingBottom: 5}}>Charge the scanner and make sure the scanner has the latest firmware by charging the scanner close to a gateway.</td>
-          </tr>
-          <tr style={{verticalAlign: "top"}}>
-            <td style={{paddingRight: 5, paddingBottom: 5}}><strong>Constantly tripping</strong></td>
-            <td style={{paddingRight: 5, paddingBottom: 5}}>The motion sensor on the scanner is constantly tripping.</td>
-            <td style={{paddingRight: 5, paddingBottom: 5}}>Blow on the center hole of the scanner with compressed air and press the reset button on the scanner.</td>
+            <td style={{paddingRight: 5, paddingBottom: 5}}>Replace the scanner with a new one (see below).</td>
           </tr>
           <tr style={{verticalAlign: "top"}}>
             <td style={{paddingRight: 5, paddingBottom: 5}}><strong>Weak Signal</strong></td>
             <td style={{paddingRight: 5, paddingBottom: 5}}>The scanner is too far away from a gateway.</td>
             <td style={{paddingRight: 5, paddingBottom: 5}}>Make sure all the gateways are plugged in and online. A gateway is online if it has a blue and red led lit up. If there are only red leds lit up, unplug it and plug it back in. If all gateways are online, you may need to move a gateway closer to this scanner or add another gateway to the gym.</td>
+          </tr>
+          <tr style={{verticalAlign: "top"}}>
+            <td style={{ paddingRight: 5, paddingBottom: 5 }}><strong>Constantly tripping / <span style={{
+              color: "#f00"
+            }}>Blinking Red Light</span></strong></td>
+            <td style={{paddingRight: 5, paddingBottom: 5}}>The motion sensor on the scanner is constantly tripping.</td>
+            <td style={{paddingRight: 5, paddingBottom: 5}}>Blow on the center hole of the scanner with compressed air and press the reset button on the scanner.</td>
           </tr>
         </tbody>
       </table>
